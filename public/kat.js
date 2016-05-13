9c9cf3059caba6c7b1add2104b306b1bc1cc2fab
SZABO Gergely <szg@subogero.com>
2016-05-14 01:04:00 +0200

    Search in webapp

--- public/kat.js
+++ public/kat.js
@@ -1,13 +1,14 @@
-kat = {};
+var kat = {};
 
 kat.search = function(query) {
-    document.getElementById("hits").innerHTML = "Waiting for kat.cr...";
+    document.getElementById("status").innerHTML = "Waiting for kat.cr...";
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         if (req.readyState == 4 && req.status == 200) {
             kat.render(JSON.parse(req.responseText));
+            document.getElementById("status").innerHTML = '200';
         } else {
-            kat.err(req);
+            document.getElementById("status").innerHTML = req.status + ' ' + req.responseText;
         }
     }
     req.open("GET", query, true);
@@ -16,11 +17,13 @@ kat.search = function(query) {
 
 kat.render = function(hitlist) {
     var table = document.createElement('TABLE');
-    table.appendChild(kat.tableheader(['', 'Size', 'Seeds', 'Title']);
+    table.appendChild(kat.tableheader(['', 'Size', 'Seeds', 'Title']));
     for (i = 0; i < hitlist.length; i++) {
         table.appendChild(kat.renderhit(hitlist[i]));
     }
-    document.getElementById("hits").innerHTML = table;
+    hits = document.getElementById("hits");
+    while (hits.firstChild) { hits.removeChild(hits.firstChild) }
+    hits.appendChild(table);
 }
 
 kat.tableheader = function(fields) {
@@ -34,21 +37,44 @@ kat.tableheader = function(fields) {
 }
 
 kat.renderhit = function(hit) {
-    var hit = document.createElement('TR');
+    var row = document.createElement('TR');
 
-    var h = document.createElement('TD');
-    var b = document.createElement('BUTTON');
-    b.onclick = kat.start(hit.magnet);
-    h.appendChild(document.createTextNode(hit.magnet));
-    hit.appendChild(h);
+    var d0 = document.createElement('TD');
+    var b = document.createElement('INPUT');
+    b.type = 'button';
+    b.value = 'Start';
+    b.setAttribute('magnet', hit.magnet);
+    b.onclick = kat.start;
+    d0.appendChild(b);
+    row.appendChild(d0);
 
-    return hit;
+    var d1 = document.createElement('TD');
+    d1.appendChild(document.createTextNode(hit.size));
+    row.appendChild(d1);
+
+    var d2 = document.createElement('TD');
+    d2.appendChild(document.createTextNode(hit.seeds));
+    row.appendChild(d2);
+
+    var d3 = document.createElement('TD');
+    d3.appendChild(document.createTextNode(hit.title));
+    row.appendChild(d3);
+
+    return row;
 }
 
-kat.err = function(req) {
-    var txt = document.createTextNode(req.status + ' ' + req.responseText);
-    document.getElementById("hits").innerHTML = txt;
-}
-
-kat.start = function(magnet) {
+kat.start = function() {
+    document.getElementById("status").innerHTML = "Starting torrent...";
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200) {
+            document.getElementById("status").innerHTML = "Torrent started";
+            kat.render(JSON.parse(req.responseText));
+        } else {
+            document.getElementById("status").innerHTML = req.status + ' ' + req.responseText;
+        }
+    }
+    req.open("POST", 'start', true);
+    req.setRequestHeader("Content-type","application/json");
+    req.send(JSON.stringify({ magnet: this.magnet }));
 }
